0014c42eca37e28f4b83d0416ebcd256d3cdf901
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-10 02:26:37 -0700

    Created handler function inside Quiz component and then set it to fire on the click of a button. Handler function expects the answer to be passed in order for it to be pushed to the State array as a new array item value. Added some additional HTML tags for structure and styling.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,16 +7,22 @@ export default function Quiz() {
 
   const activeQuestionIndex = userAnswers.length;
 
+  const handleSelectAnswer = selectedAnswer => {
+    setUserAnswers(prevUserAnswers => [ ...prevUserAnswers, selectedAnswer ]);
+  };
+
   return (
-    <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer, i) => (
-          <li key={i} className="answer">
-            <button>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer, i) => (
+            <li key={i} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
