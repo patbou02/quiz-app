a7d9ffb249c8bd03b358da9c21b3d0ccaa89884b
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-10 02:03:35 -0700

    Connected questions.js file to the Quiz component via import and then used it to fetch the text and list of answers corresponding to each question based off of the activeQuestionIndex which initially is just 0. Imported Quiz component into App root component.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
   return (
-    <p>Currently active question:</p>
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer, i) => <li key={i}>{answer}</li>)}
+      </ul>
+    </div>
   );
 }
