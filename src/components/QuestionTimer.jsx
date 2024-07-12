625e38f9c2b55e9b4258e8df54a6ab1c1dc32bdc
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-11 22:43:49 -0700

    Added timeout and interval logic to update a State value within the QuestionTimer component which expects a timeout and onTimeout function as props. Add timer to Quiz component.

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,5 +1,20 @@
-export default function QuestionTimer({ timeLeft }) {
+import { useState, useEffect } from 'react';
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+    }, 100);
+  }, []);
+
   return (
-    <progress id="question-time" />
+    <progress id="question-time" max={timeout} value={remainingTime} />
   );
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -28,6 +29,7 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={15000} onTimeout={() => handleSelectAnswer(null)} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer, i) => (
