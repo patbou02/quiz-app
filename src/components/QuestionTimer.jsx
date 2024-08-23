0dd8e9c9c664a0e361ed821089b97b98e2ec25ca
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-08-22 21:44:52 -0700

    Fixed interval from not properly getting updated because of duplicate interval being run in the background by returning cleanup function which clears the interval. Doing the same so that no additional timeouts are running in teh background either. Added key to the QuestionTimer within the Quiz component in order to force the component to re-render at the end of each timer.

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -5,15 +5,25 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log('SETTING TIMEOUT.');
-    setTimeout(onTimeout, timeout);
+    console.log('SETTING TIMEOUT');
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      console.log('CLEARING TIMEOUT');
+      clearTimeout(timer);
+    }
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log('SETTING INTERVAL.');
-    setInterval(() => {
+    console.log('SETTING INTERVAL');
+    const interval = setInterval(() => {
       setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      console.log('CLEARING INTERVAL');
+      clearInterval(interval);
+    };
   }, []);
 
   return (
